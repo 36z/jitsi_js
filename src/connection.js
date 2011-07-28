d9a8b1e02899ced28c85981006f6a6127b9be22d
Oren Forer <oren@junctionnetworks.com>
2011-07-28 12:06:58 -0400

    no enhancements just fixed some syntax issues

--- src/connection.js
+++ src/connection.js
@@ -2,7 +2,7 @@ Jitsi.Mixin = {};
 
 Jitsi.Service = {};
 
-Jitsi.Mixin.RegistrationHandler = {
+Jitsi.Mixin.RegistrationHandler = Jitsi.Base.extend({
 
   init: Jitsi.Function.around(
     function($super) {
@@ -44,6 +44,12 @@ Jitsi.Mixin.RegistrationHandler = {
     return true;
   },
 
+  unregisterHandler: function (event) {
+    if (this._registeredHandlers[event]) {
+      delete this._registeredHandlers[event];
+    }
+  },
+
   fireHandler: function (event) {
     if (this._registeredHandlers[event]) {
       var action = this._registeredHandlers[event].action,
@@ -52,11 +58,27 @@ Jitsi.Mixin.RegistrationHandler = {
     }
   }
 
-};
+});
 
+Jitsi.Service.Api = {
+  Calls: {
+    CREATE: "CALL_CREATE",
+    TERMINATE: "CALL_TERMINATE",
+    REQUESTED: "CALL_REQUESTED",
+    SEND_TONE: "SEND_TONE",
+    MUTE: "MUTE",
+    HOLD: "HOLD"
+  },
+  Register: {
+    REGISTER: "REGISTER",
+    UNREGISTER: "UNREGISTER"
+  }
+};
 
 Jitsi.Service.Register = Jitsi.Base.extend (
   Jitsi.Mixin.RegistrationHandler, {
+
+  api: Jitsi.Service.Api.Register,
 
   init: Jitsi.Function.around(
     function($super) {
@@ -82,26 +104,42 @@ Jitsi.Service.Register = Jitsi.Base.extend (
    *   this.fireHandler('onRegistered',args)
    */
   _handleRegisterEvents: function(regEvent) {
-    throw new Error("Must Implement");
+    var jevt = null;
+    var type = null;
+    if (regEvent) {
+      if (regEvent.constructor == String) {
+        jevt = JSON.parse(regEvent);
+      } else if (regEvent.constructor == Object) {
+        jevt = regEvent;
+      }
+    }
+    if (jevt && jevt.type) {
+      type = jevt.type;
+      this.fireHandler('onRegisterEvent', jevt);
+      return this;
+    }
+    throw new Jitsi.Error("Could not parse register event");
   },
 
   /**
    * example:
    *   this.connection.sendEvent('register',username, pwd, ...);
    */
-  register: function() {
-    throw new Error("Must Implement");
+  register: function(username, displayName, authUsername, passwd) {
+      return this.connection.sendEvent(this.api.REGISTER,
+                                       [username, displayName,
+                                        authUsername, passwd]);
   },
 
   unregister: function() {
-    throw new Error("Must Implement");
+    return this.connection.sendEvent(this.api.UNREGISTER, []);
   }
 
 });
 
-
-Jitsi.Service.Call = Jitsi.Base.extend (
-  Jitsi.Mixin.RegistrationHandler, {
+Jitsi.Service.Call = Jitsi.Base.extend(Jitsi.Mixin.RegistrationHandler,
+{
+  api: Jitsi.Service.Api.Calls,
 
   init: Jitsi.Function.around(
     function($super) {
@@ -132,36 +170,71 @@ Jitsi.Service.Call = Jitsi.Base.extend (
    *
    */
   _handleCallEvents: function(callEvent) {
-    throw new Error("Must Implement");
+    var jevt = null;
+    var type = null;
+    if (callEvent) {
+      if (callEvent.constructor == String) {
+        jevt = JSON.parse(callEvent);
+      } else if (callEvent.constructor == Object) {
+        jevt = callEvent;
+      }
+    }
+    if (jevt && jevt.type) {
+      type = jevt.type;
+      console.log('about to call onCallEvent');
+      this.fireHandler('onCallEvent', jevt);
+      return this;
+    }
+    throw new Jitsi.Error("Could not parse callEvent");
   },
 
   /**
-   * example:
-   *   this.connection.sendEvent('create',sip);
+   * Make a call
+   * @param {String} SIP Uri
    */
   create: function(sip) {
-    throw new Error("Must Implement");
+    console.log('API: ' + this.api);
+    return this.connection.sendEvent(this.api.CREATE, [sip]);
   },
 
   /**
-   * example:
-   *   this.connection.sendEvent('hangup',peerId);
+   * Hang-up a call
    */
   hangup: function() {
-    throw new Error("Must Implement");
+    // TODO: support peerId
+    return this.connection.sendEvent(this.api.HANGUP, []);
   },
 
-  hold: function() {
-    throw new Error("Must Implement");
+  /**
+   * Hold
+   */
+  hold: function(bHold) {
+    if (bHold){
+      bHold = bHold.toString().trim();
+      if (bHold == 'true' || bHold == 'false'){
+        return this.connection.sendEvent(this.api.HOLD, [bHold]);
+      }
+    }
+    throw new Jitsi.Error('Invalid parameter for hold , should be boolean');
+  },
+
+  /**
+   * DTMF tones
+   */
+  sendTone: function(key) {
+    if(key){
+      key = key.toString().charAt(0);
+      if(/[0-9]|\*|\#/.test(key)){
+        return this.connection.sendEvent(this.api.SEND_TONE, [key]);
+      }
+    }
+    throw new Jitsi.Error('Invalid parameter key for sendTone');
   },
 
   transfer: function() {
-    throw new Error("Must Implement");
-  },
-
-  sendTone: function(key) {
-    throw new Error("Must Implement");
+    throw new Jitsi.Error("Future Implementation");
   }
+
 });
 
 Jitsi.Service.Loader = Jitsi.Base.extend (
@@ -190,8 +263,22 @@ Jitsi.Service.Loader = Jitsi.Base.extend (
    * example:
    *   this.fireHandler('onLoading',args)
    */
-  _handleLoadEvents: function() {
-    throw new Error("Must Implement");
+  _handleLoadEvents: function(loadEvent) {
+    var jevt = null;
+    var type = null;
+    if (loadEvent) {
+      if (loadEvent.constructor == String) {
+        jevt = JSON.parse(loadEvent);
+      } else if (loadEvent.constructor == Object) {
+        jevt = loadEvent;
+      }
+    }
+    if (jevt && jevt.type) {
+      type = jevt.type;
+      this.fireHandler('onLoadEvent', jevt);
+      return this;
+    }
+    throw new Jitsi.Error("Could not parse loadEvent");
   }
 });
 
@@ -248,6 +335,7 @@ Jitsi.Connection = Jitsi.Base.extend({
   _dispatchEvent: function(rawEvent) {
     var callbacks = this._findCallbacks(rawEvent),
       cbLen = callbacks.length;
+
     for (var i=0;i<cbLen;i++) {
       callbacks[i].call(null,rawEvent);
     }
@@ -257,7 +345,7 @@ Jitsi.Connection = Jitsi.Base.extend({
    * Make a call into the applet via the appletAdapter
    */
   sendEvent: function(event) {
-    this.appletAdapter.sendEvent(event);
+    return this.appletAdapter.sendEvent(event);
   },
 
   /**
